e9151f19fc3a23bac8d8fbf08aa9f8925793369d
PedroCarbonaroG <pedrocarbonaro73@gmail.com>
2024-03-01 14:23:19 -0300

    SE10_InitialClasses_Created

---
+++ JavaPOO/SolvedExercise10_TratamentoManipulacaoArquivos/src/entities/Product.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class Product {
+    
+    private String name;
+    private double price;
+    private short quantity;
+
+    public Product() {
+    }
+    public Product(String name, double price, short quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public short getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setQuantity(short quantity) {
+        this.quantity = quantity;
+    }
+
+    private double finalPrice() {
+        return price * quantity;
+    }
+
+    @Override
+    public final String toString() {
+        return name + "," + finalPrice();
+    }
+
+}
