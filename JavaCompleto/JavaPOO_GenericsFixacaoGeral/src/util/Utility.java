02709cf6098b2f7f16fc4532526d0fc4fc779771
PedroCarbonaroG <pedrocarbonaro73@gmail.com>
2024-03-08 16:48:44 -0300

    JavaPOO_GenericsFixacaoGeralNotDoneYet

---
+++ JavaCompleto/JavaPOO_GenericsFixacaoGeral/src/util/Utility.java
@@ -0,0 +1,36 @@
+package util;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public abstract class Utility {
+
+
+    public static <T> Set<T> findUniqueElements(List<T> list) {
+        return new HashSet<>(list);
+        /*Can be used like this:
+            
+            Set<T> setList = new HashSet<>();
+            for(T t : list) {
+                setList.add(t);
+            }
+            return setList;
+        */
+    }
+
+    public static Map<String, Integer> countWords(String sequence) {
+        Map<String, Integer> list = new TreeMap<>();
+        String[] wordsList = sequence.split(" ");
+        for (String s : wordsList) {
+            list.put(s, list.containsKey(s) ? list.get(s) + 1 : 1);
+        }
+        return list;
+    }
+
+    public static <T> Map<T, Integer> countFrequency(List<T> list) {
+        return null;
+    }
+}
---
+++ JavaCompleto/JavaPOO_GenericsFixacaoGeral/src/view/Prompt.java
@@ -0,0 +1,81 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.TreeMap;
+
+import util.Utility;
+
+public class Prompt {
+
+    /*
+
+    ** First Exercise.: Implement a class called UniqueElements that
+    takes a list of elements of any type and returns a HashSet
+    containing only the unique elements. Use Generics to make the
+    flexible class for different data types.
+
+    ** Second Exercise.: Create a class called SortedUniqueWords that 
+    takes a phrase as input and returns an alphabetically ordered 
+    TreeMap, where the words are the keys and the number of 
+    occurrences of each word is the associated value. Use 
+    LinkedHashSet to maintain the order of insertion of words in the
+    sentence.
+
+    ** Third Exercise.: Create a method called FrequencyCounter that 
+    takes a list of integers and returns a LinkedHashMap where the 
+    keys are the unique numbers in the list and the values ​​are their 
+    frequencies. Use TreeSet to ensure that the keys in the map 
+    are in ascending order.
+
+    */
+
+    //EX01
+    private List<Integer> integersList01 = new ArrayList<>();
+    private List<String> stringList01 = new ArrayList<>();
+    private List<Double> doubleList01 = new ArrayList<>();
+
+    //EX03
+    private List<Integer> integersList03 = new ArrayList<>();
+    private List<String> stringList03 = new ArrayList<>();
+    private List<Double> doubleList03 = new ArrayList<>();
+
+    public void startPrompt() {
+
+        System.out.println("===========================================================");
+
+        //EX01
+        System.out.println("Results of Exercise 1: ");
+        integersList01.addAll(Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3));
+        stringList01.addAll(Arrays.asList("Piter", "Jonas" , "Lisa", "Lisa", "Fred", "Piter", "Jonas"));
+        doubleList01.addAll(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 1.0, 2.0, 3.0));
+
+        System.out.println(new HashSet<>(Utility.findUniqueElements(integersList01)).toString());
+        System.out.println(new HashSet<>(Utility.findUniqueElements(stringList01)).toString());
+        System.out.println(new HashSet<>(Utility.findUniqueElements(doubleList01)).toString());
+        System.out.println();
+        //Can be used like this too
+        // System.out.println(new HashSet<>(integersList01));
+
+        //EX02
+        System.out.println("Results of Exercise 2: ");
+        System.out.println(new TreeMap<>(Utility.countWords("java programming is java fun is programming")));
+        System.out.println(new TreeMap<>(Utility.countWords("yesterday i remebered that yesterday was good as cream that i enjoyed")));
+        System.out.println(new TreeMap<>(Utility.countWords("for all days i need all days a good thing to think for")));
+        System.out.println();
+
+        //EX03
+        System.out.println("Results of Exercise 3: ");
+        integersList01.addAll(Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3));
+        stringList01.addAll(Arrays.asList("Piter", "Jonas" , "Lisa", "Lisa", "Fred", "Piter", "Jonas"));
+        doubleList01.addAll(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 1.0, 2.0, 3.0));
+
+        System.out.println(new LinkedHashMap<>(Utility.countFrequency(integersList01)).toString());
+        System.out.println(new LinkedHashMap<>(Utility.countFrequency(stringList01)).toString());
+        System.out.println(new LinkedHashMap<>(Utility.countFrequency(doubleList01)).toString());
+        System.out.println();
+    }
+}
