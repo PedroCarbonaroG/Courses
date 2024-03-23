b3cd70ac4cd00d7689b6670373ec0747111e3984
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-03-22 21:51:16 -0300

    JDBCProject_DataRecoveryDone

--- JavaFull/JDBC_Project/src/app/App.java
+++ JavaFull/JDBC_Project/src/app/App.java
@@ -1,12 +1,54 @@
 package app;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import DB.DB;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+
+        PreparedStatement ps;
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("Select * from department");
+            
+            System.out.println("Departments:");
+            while (rs.next()) { System.out.println(rs.getInt("Id") + ", " + rs.getString("Name")); }
+            System.out.println();
+
+            rs = st.executeQuery("Select * from seller");
+            System.out.println("Sellers:");
+            while (rs.next()) { 
+                System.out.println(
+                    rs.getInt("Id") + ", " + 
+                    rs.getString("Name") + ", " +
+                    rs.getString("Email") + ", " +
+                    rs.getDate("BirthDate") + ", " +
+                    rs.getDouble("BaseSalary") + ", " +
+                    rs.getInt("DepartmentId")
+                    ); 
+            }
+
+            
+
+        }
+        catch (SQLException e) { e.printStackTrace(); }
+        finally {
+            conn.close();
+            st.close();
+            rs.close();
+        }
+
     }
 }
