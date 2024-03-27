10e6e925deaefe878b1d87856ed061087c64dd22
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-03-27 15:52:51 -0300

    JDBC_Project - feat: all methods implementated to DepartmentDaoJDBC

--- JavaFull/JDBC_Project/src/model/dao/impl/DepartmentDaoJDBC.java
+++ JavaFull/JDBC_Project/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,11 +1,17 @@
 package model.dao.impl;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 
 import model.dao.DepartmentDao;
+
 import model.entities.Department;
 
 public class DepartmentDaoJDBC extends DB implements DepartmentDao {
@@ -32,27 +38,92 @@ public class DepartmentDaoJDBC extends DB implements DepartmentDao {
     }
 
     @Override
-    public boolean update(Department dp) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    public boolean update(Department department) throws SQLException {
+        boolean updated = false;
+
+        ps = conn.prepareStatement(
+            "UPDATE department " +
+            "SET Name = ? " +
+            "WHERE id = ?"
+        );
+
+        ps.setString(1, department.getName());
+        ps.setInt(2, department.getId());
+
+        ps.executeUpdate();
+        updated = true;
+
+        return updated;
     }
 
     @Override
     public boolean deleteById(Integer id) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+        boolean deleted = false;
+
+        ps = conn.prepareStatement(
+            "DELETE FROM department " +
+            "WHERE Id = ?"
+        );
+
+        ps.setInt(1, id);
+
+        ps.executeUpdate();
+        deleted = true;
+
+        return deleted;
     }
 
     @Override
     public Department findById(Integer id) throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        
+        ps = conn.prepareStatement(
+            "SELECT * FROM department WHERE Id = ?"
+        );
+
+        ps.setInt(1, id);
+        rs = ps.executeQuery();
+
+        if (rs.next()) {
+
+            Department dp = instantiateDepartment(rs);
+
+            return dp;
+        }
+
+        return null;
     }
 
     @Override
     public List<Department> findAll() throws SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        
+        List<Department> list = new ArrayList<>();
+        Map<Integer, Department> map = new HashMap<>();
+
+        ps = conn.prepareStatement(
+            "SELECT * FROM department ORDER BY id"
+        );
+
+        rs = ps.executeQuery();
+
+        while (rs.next()) {
+
+            Department dp = map.get(rs.getInt("Id"));
+
+            if (dp == null) {
+                dp = instantiateDepartment(rs);
+                map.put(rs.getInt("Id"), dp);
+            }
+
+            list.add(new Department(instantiateDepartment(rs)));
+        }
+
+        return list;
     }
     
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dp = new Department();
+        dp.setId(rs.getInt("Id"));
+        dp.setName(rs.getString("Name"));
+        return dp;
+    }
 }
--- JavaFull/JDBC_Project/src/view/Prompt.java
+++ JavaFull/JDBC_Project/src/view/Prompt.java
@@ -9,6 +9,7 @@ import db.DB;
 import db.DbException;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 
 import model.entities.Department;
@@ -21,26 +22,29 @@ public class Prompt {
         try {
 
             //Auxiliar attributes to tests
-            List<Seller> list;
-            SellerDao sellerDao;
+            List<Seller> sellerList;
+            SellerDao sellerDao = DaoFactory.createSellerDao();;
             Seller seller;
 
+            List<Department> departmentList;
+            DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+            Department department;
+
             System.out.println("======================= TEST 1 - FIND SELLER BY ID =======================");
-            sellerDao = DaoFactory.createSellerDao();
             seller = sellerDao.findById(3);
             System.out.println(seller);
 
             System.out.println("\n======================= TEST 2 - FIND SELLER BY DEPARTMENT =======================");
-            list = sellerDao.findByDepartment(seller.getDepartment());
-            list.forEach(x -> System.out.println(x));
+            sellerList = sellerDao.findByDepartment(seller.getDepartment());
+            sellerList.forEach(x -> System.out.println(x));
 
             System.out.println("\n======================= TEST 3 - FIND SELLER BY DEPARTMENT ID =======================");
-            list = sellerDao.findByDepartmentId(2);
-            list.forEach(x -> System.out.println(x));
+            sellerList = sellerDao.findByDepartmentId(2);
+            sellerList.forEach(x -> System.out.println(x));
 
             System.out.println("\n======================= TEST 4 - FIND ALL SELLERS =======================");
-            list = sellerDao.findAll();
-            list.forEach(x -> System.out.println(x));
+            sellerList = sellerDao.findAll();
+            sellerList.forEach(x -> System.out.println(x));
 
             System.out.println("\n======================= TEST 5 - INSERT INTO SELLER =======================");
             seller = new Seller(null, "piter", "piter@gmail.com", new Date(), 5000, new Department(2, null));
@@ -59,6 +63,29 @@ public class Prompt {
             if (sellerDao.deleteById(7)) {
                 System.out.println("Deleted!");
             }
+
+            System.out.println("\n======================= TEST 8 - FIND DEPARTMENT BY ID =======================");
+            System.out.println(departmentDao.findById(1));
+
+            System.out.println("\n======================= TEST 9 - FIND ALL DEPARTMENTS =======================");
+            departmentList = departmentDao.findAll();
+            departmentList.forEach(x -> System.out.println(x));
+
+            System.out.println("\n======================= TEST 10 - ADD DEPARTMENT =======================");
+            departmentDao.add(new Department(null, "Food"));
+            System.out.println(departmentDao.findById(5));
+
+            System.out.println("\n======================= TEST 11 - UPDATE DEPARTMENT =======================");
+            department = departmentDao.findById(5);
+            department.setName("Music");
+            departmentDao.update(department);
+            System.out.println(departmentDao.findById(5));
+
+            System.out.println("\n======================= TEST 12 - DELETE DEPARTMENT =======================");
+            if (departmentDao.deleteById(5)) {
+                System.out.println("Deleted!");
+            }
+
         }
 
         catch (SQLException e) { throw new DbException(e.getMessage()); }
